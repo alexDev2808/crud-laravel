1af070437c6b8b0f7df40698aa1742cc4e98ef93
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-14 16:16:24 -0600

    Insertando datos y guardar fotos

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,1 +1,22 @@
-Formulario de creacion de empleado
+{{-- Formulario de creacion de empleado --}}
+
+<form action="{{ route('empleado.store') }}" method="post" enctype="multipart/form-data">
+  @csrf  
+  <label for="Nombre">Nombre</label>  
+  <input type="text" name="Nombre" id="Nombre">
+
+  <label for="ApellidoPaterno">Apellido Paterno</label>  
+  <input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+
+  <label for="ApellidoMaterno">Apellido Materno</label>  
+  <input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+
+  <label for="Correo">Correo</label>  
+  <input type="text" name="Correo" id="Correo">
+
+  <label for="Foto">Foto</label>  
+  <input type="file" name="Foto" id="Foto">
+
+  <input type="submit" value="GuardarDatos">
+
+</form>

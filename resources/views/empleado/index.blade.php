e7d3140729f1e1c40f194394fa584ef968486540
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-14 16:34:33 -0600

    Vista para consultar datos

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,40 @@
 Mostrar lista de empleados
+
+<div class="table-responsive-md">
+    <table class="table table-striped
+    table-hover	
+    table-borderless
+    table-light
+    align-middle">
+        <thead class="table-light">
+            <caption>Table Name</caption>
+            <tr>
+                <th>#</th>
+                <th>Foto</th>
+                <th>Nombre</th>
+                <th>Apellido Paterno</th>
+                <th>Apellido Materno</th>
+                <th>Correo</th>
+                <th>Acciones</th>
+            </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @foreach ($empleados as $empleado)
+                    
+                <tr class="table-primary" >
+                    <td>{{ $empleado->id }}</td>
+                    <td>{{ $empleado->Foto }}</td>
+                    <td>{{ $empleado->Nombre }}</td>
+                    <td>{{ $empleado->ApellidoPaterno }}</td>
+                    <td>{{ $empleado->ApellidoMaterno }}</td>
+                    <td>{{ $empleado->Correo }}</td>
+                    <td>Editar | Borrar</td>
+                </tr>
+
+                @endforeach
+            </tbody>
+            <tfoot>
+                
+            </tfoot>
+    </table>
+</div>

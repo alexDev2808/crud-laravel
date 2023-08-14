1bc27e8648b766f338c77aa07a7310182d792b12
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-14 17:02:05 -0600

    Formularion de edicion

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,18 +1,19 @@
 {{-- Formulario que tendra los datos en comun con create y edit --}}
 
 <label for="Nombre">Nombre</label>
-<input type="text" name="Nombre" id="Nombre">
+<input type="text" name="Nombre" id="Nombre" value="{{ $empleado->Nombre }}">
 
 <label for="ApellidoPaterno">Apellido Paterno</label>
-<input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="{{ $empleado->ApellidoPaterno }}">
 
 <label for="ApellidoMaterno">Apellido Materno</label>
-<input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="{{ $empleado->ApellidoMaterno }}">
 
 <label for="Correo">Correo</label>
-<input type="text" name="Correo" id="Correo">
+<input type="text" name="Correo" id="Correo" value="{{ $empleado->Correo }}">
 
 <label for="Foto">Foto</label>
-<input type="file" name="Foto" id="Foto">
+{{ $empleado->Foto }}
+<input type="file" name="Foto" id="Foto" value="">
 
 <input type="submit" value="GuardarDatos">

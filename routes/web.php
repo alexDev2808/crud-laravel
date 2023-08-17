55fd3b3c3c2d8a1aaa24098eae76ceffc1e3ab15
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-17 08:01:53 -0600

    Configurando autenticacion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmpleadoController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 // Route::get('/empleado', function () {
 //     return view('empleado.index');
@@ -25,4 +26,8 @@ Route::get('/', function () {
 Route::resource('empleado', EmpleadoController::class);
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [EmpleadoController::class, 'index'])->name('home');
+});

53b9d9991e54a62d21e6f9b5faf083cc1c02bcab
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-17 07:53:10 -0600

    Agregando bootstrap

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/', function () {
 // Route::get('/empleado/create', [EmpleadoController::class, 'create']);
 
 Route::resource('empleado', EmpleadoController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
